1abcac8cee7bcf2f68d1907ba42d7702ed306141
wanbei <liuluopingyue@sina.com>
2016-03-03 18:21:00 +0800

    Delete gototop.js

--- gototop.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' to the page top!');
-  chrome.tabs.executeScript({
-    code: 'document.body.scrollTop=0'
-  });
-});
